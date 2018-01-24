3609d79449ba06a75da51a6b29feac7f5bb78dbd
Miguel <Malave232@gmail.com>
2018-01-24 12:01:22 -0500

    Calculator Project

---
+++ Calculator/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int firstnum;
+			int secondnum;
+			int operation;
+			int answer;
+
+			Console.WriteLine("Welcome to my calc");
+
+			Console.WriteLine("Enter the first number in your basic equation:");
+
+			firstnum = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Now enter your second number:");
+
+			secondnum = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter operation. (+,-,/,*):");
+			operation = Convert.ToInt32(Console.ReadLine());
+
+			switch (operation)
+
+
+			{
+				case 1:
+
+					answer = firstnum - secondnum;
+					break;
+
+
+				case 2:
+
+					answer = firstnum + secondnum;
+					break;
+
+
+				case 3:
+
+					answer = firstnum / secondnum;
+					break;
+
+
+				case 4:
+
+					answer = firstnum * secondnum;
+					break;
+
+
+				default:
+
+					answer = 0;
+					break;
+
+			}
+			{
+				Console.WriteLine(firstnum.ToString() + "" + operation + "" + secondnum.ToString() + " = " + answer.ToString());
+
+
+				Console.ReadLine();
+				
+			}
+
+        }
+    }
+}
